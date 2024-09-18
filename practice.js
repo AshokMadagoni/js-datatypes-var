3d61d95d111fc4b36a7aaea33ee0627a828a444a
AshokMadagoni <madagoniashok1@gmail.com>
2024-09-18 20:04:26 +0530

    added

---
+++ practice.js
@@ -0,0 +1,30 @@
+var a=10;
+var job="developer"
+var sal=999;
+
+var details={
+    name:"ashok",
+    age:20,
+    currentmovie:"devaraaa",
+}
+
+var ashok=[20,"name",true,false,{name:"hlo",gender:"male",package:555},[1,2,3]]
+
+console.log(a);
+console.log(job);
+console.log(sal);
+
+console.log(details);
+console.log(details.age);
+
+console.log(ashok);
+console.log(ashok[2]);
+console.log(ashok.length);
+
+
+
+
+
+
+
+
